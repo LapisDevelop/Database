896c543fb902d42b05653e610cd517ebdc51130d
dylancode <opensource@dylancode.dev>
2026-06-19 09:49:03 +0100

    change LapisDatabase version to 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,16 +3,14 @@ plugins {
 }
 
 group = "com.lapisdev"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.test {
