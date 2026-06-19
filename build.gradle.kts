d43680d69f271c69133f89687d635d7a7f32d035
dylancode <opensource@dylancode.dev>
2026-06-19 12:02:27 +0100

    update to 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,13 @@ java {
     toolchain.languageVersion = JavaLanguageVersion.of(25)
 }
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:24.0.1")
+}
+
 group = "com.lapisdev"
-version = "0.0.3"
+version = "0.1.0"
